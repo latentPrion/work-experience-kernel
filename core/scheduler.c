1d44427019c410b1a811a5387aa512f128270824
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2019-04-08 02:23:20 +1000

    Add scheduler.c and reg_context.h

---
+++ core/scheduler.c
@@ -0,0 +1,5 @@
+
+int scheduler_init(scheduler_t *s)
+{
+    // Init the run queue linked list.
+}
---
+++ include/arch/ia32/reg_context.h
@@ -0,0 +1,7 @@
+#ifndef _WEK_REG_CONTEXT_H
+#define _WEK_REG_CONTEXT_H
+
+typedef struct reg_context_ {
+} reg_context_t;
+
+#endif
